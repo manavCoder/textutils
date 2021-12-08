97d76cdb1d29a86114c5f3354a8826ca2ef6dff6
Manav <anonymous.manav@gmail.com>
2021-12-09 00:25:37 +0530

    Video 6 completed

---
+++ module1.mjs
@@ -0,0 +1,9 @@
+const a = "Akash"
+const b = "Madhu"
+const c = "Brodada"
+const d = "Daddy"
+
+export default d;  // default export
+export {a};  // named export
+export {b};
+export {c};
---
+++ module2.mjs
@@ -0,0 +1,4 @@
+import ui, {a, b, c} from './module1.mjs'
+console.log(ui);
+console.log(a);
+console.log(c);
